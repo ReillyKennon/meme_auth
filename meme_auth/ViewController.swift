89bb16388733f45aa68ff4f762219582bcebed26
Reilly kennon <reilly@Reillys-MacBook-Pro.local>
2017-07-19 14:58:00 -0700

    pods for auth stuff installed and very basic sign page

--- meme_auth/ViewController.swift
+++ meme_auth/ViewController.swift
@@ -7,9 +7,48 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuthUI
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, FUIAuthDelegate {
+    
+    
+
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //Listen for signed in user
+        let authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+        
+        
+    
+
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if Auth.auth().currentUser == nil {
+                
+                let authViewController = authUI!.authViewController()
+                self.present(authViewController, animated: true, completion: nil)
+            }
+            
+            
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        Auth.auth().removeStateDidChangeListener(handle!)
+        
+    }
+
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +59,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        
+    }
+    
 
+    
+    
+
+//    extension ViewController: FUIAuthDelegate {
+//        func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error?) {
+//            print("did sign in with \(String(describing: user)) and errors? \(String(describing: user)) and errors? \(String(describing: error))")
+//        }
+//    }
+    
 }
 
